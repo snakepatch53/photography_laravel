9f20226c6d88bbb99dcd45987533d4f77f2b97c8
snakepatch53 <snakepatch53@gmail.com>
2023-12-19 16:06:18 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   public/css.landing/video360.css
    	modified:   resources/views/landing/video360.blade.php

--- resources/views/landing/video360.blade.php
+++ resources/views/landing/video360.blade.php
@@ -3,9 +3,64 @@
 @section('title', 'Video 360')
 
 @section('head')
-    <link rel="stylesheet" href="{{ asset('/public/css.landing/blog.css') }}">
+    <link rel="stylesheet" href="{{ asset('/public/css.landing/video360.css') }}">
 @endsection
 
 @section('content')
-    Video 360
+    <section class="video360">
+        <div class="container">
+            <h3>Video 360</h3>
+            <p class="subtitle">Immerse yourself in a Total Experience with our 360 Video Service</p>
+
+            <div class="content">
+                <p>
+                    At Gonzalo Producciones, we invite you to explore a new level of visual immersion with our 360 Video
+                    service.
+                    Capture your most precious moments in a panoramic experience, allowing you to relive every detail from
+                    every
+                    angle.
+                </p>
+                <b>Featured Features:</b>
+                <p>
+                    <b>1. Total Panoramic:</b> Our 360 Video service offers a complete view of your surroundings. From
+                    exciting events
+                    to breathtaking landscapes, every moment is transformed into an interactive experience that you can
+                    explore
+                    at your own pace.
+                </p>
+                <p>
+                    <b> 2. Advanced Technology:</b> We use state-of-the-art equipment to capture 360-degree videos with
+                    exceptional
+                    quality. Every detail, movement and emotion is recorded with precision, offering an immersive visual
+                    experience that goes beyond conventional videography.
+                </p>
+                <p>
+                    <b>3. Personalized Interactivity:</b> You don't just watch, you interact. We customize your 360 videos
+                    to fit your
+                    specific needs. Whether for corporate events, tourism or family memories, we create an interactive
+                    experience that aligns with your vision and purpose.
+                </p>
+                <p>
+                    <b>4. Universal Compatibility:</b> Our 360 videos are compatible with a wide variety of platforms and
+                    devices. From
+                    computers to mobile devices to virtual reality headsets, we ensure your memories are accessible to
+                    everyone.
+                </p>
+                <p>
+                    <b>5. Virtual Immersion:</b> Experience the magic of virtual immersion. With 360 Video technology, we
+                    transport you
+                    to the heart of your memories, creating a deeper connection with each captured moment.
+                </p>
+                <p>
+                    At Gonzalo Producciones, we believe that moments should not just be seen, but lived. With our 360 Video
+                    service, we offer you the opportunity to explore your memories in a whole new way.
+                </p>
+                <p>
+                    Contact us today to find out how we can transform your events into interactive and unforgettable visual
+                    experiences!
+                </p>
+            </div>
+
+        </div>
+    </section>
 @endsection
